628820a77d0aade66859ba4bcbb70814de4f4bdb
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-12-11 11:44:01 +0300

    Select 'alx_book_store' database in SQL script
    Added a command to select the 'alx_book_store' database before listing tables.

--- task_3.sql
+++ task_3.sql
@@ -7,5 +7,8 @@ SELECT TABLE_NAME
 FROM INFORMATION_SCHEMA.TABLES
 WHERE TABLE_SCHEMA = DATABASE();
 
+-- Select the database
+USE alx_book_store;
+
 -- List all tables in the current database
 SHOW TABLES;
