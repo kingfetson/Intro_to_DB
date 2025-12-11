1a4689ded39151420c1c963b36de0d7740e3ee8c
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-12-11 11:40:59 +0300

    Add SQL script to list all tables in database

---
+++ task_3.sql
@@ -0,0 +1,8 @@
+-- ================================
+-- File: task_3.sql
+-- ================================
+
+-- List all tables in the current database
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = DATABASE();
